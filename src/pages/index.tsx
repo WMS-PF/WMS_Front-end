19d485ccbac5cd4dd1debface9694668c0174fe2
nayelio <nayelisortegapalma@gmail.com>
2023-04-24 19:23:48 -0500

    Temporary solution to hydration problem: ssr false for income and shipping order information.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,22 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import IncomeOrder from "@/Components/molecules/Income_Order";
-import ShippingOrder from "@/Components/molecules/Shipping_Order";
 import SearchBar from "@/Components/molecules/SearchBar";
 import OrderList from "@/Components/molecules/OrderList";
+import dynamic from "next/dynamic";
 const inter = Inter({ subsets: ["latin"] });
+const IncomeOrder = dynamic(
+  () => import("@/Components/molecules/Income_Order"),
+  {
+    ssr: false,
+  }
+);
+const ShippingOrder = dynamic(
+  () => import("@/Components/molecules/Shipping_Order"),
+  {
+    ssr: false,
+  }
+);
 
 export default function Home() {
   return (
