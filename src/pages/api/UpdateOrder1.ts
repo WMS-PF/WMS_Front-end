3958a0e68346397b5e70ad1cf8085b79d68b145f
A-FlorezMeyer <116691882+A-FlorezMeyer@users.noreply.github.com>
2023-05-16 00:48:31 -0500

    new functionality to update Status

---
+++ src/pages/api/UpdateOrder1.ts
@@ -0,0 +1,36 @@
+import { OutgoingOrders } from "./Database/models/OutgoingOrders";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+
+
+export async function updateOrderStatus2to3(
+    req: NextApiRequest,
+    res: NextApiResponse
+  ) {
+    if (req.method === "PUT") {
+      try {
+        const { OrderID } = req.body;
+  
+        // Buscar la orden con el ID correspondiente y con estado 2
+        const orderToUpdate = await OutgoingOrders.findOne({
+          where: { OrderID: OrderID },
+        });
+  
+        if (orderToUpdate) {
+          // Actualizar el estado de la orden
+          orderToUpdate.Status = 2;
+  
+          // Guardar los cambios en la base de datos
+          await orderToUpdate.save();
+        }
+  
+        res.status(200).json({ message: "Orden actualizada con éxito" });
+      } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: `Error al actualizar la orden: ${error}` });
+      }
+    } else {
+      res.status(405).json({ message: "Método no permitido" });
+    }
+  }
+  
